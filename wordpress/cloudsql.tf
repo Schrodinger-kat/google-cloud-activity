370a99cf213b8013fd21d328ea9293bbd741e112
jishnn tp <jishnn.tp@gmail.com>
2021-02-18 00:32:56 +0530

    sql fixes

--- wordpress/cloudsql.tf
+++ wordpress/cloudsql.tf
@@ -1,11 +1,5 @@
-/*
-resource "google_sql_database" "pss" {
-  name     = "wordpress-db"
-  instance = google_sql_database_instance.pokedex3.self_link
-}
-
-resource "google_sql_database_instance" "pokedex3" {
-  name   = "jishnn-wordpress-db"
+resource "google_sql_database_instance" "pokedex4" {
+  name   = "jishnn-wpdb"
   region = "us-central1"
   settings {
     tier = "db-f1-micro"
@@ -14,15 +8,18 @@ resource "google_sql_database_instance" "pokedex3" {
   deletion_protection  = "false"
 }
 
+resource "google_sql_database" "pss" {
+  name     = "wordpressdb"
+  instance = google_sql_database_instance.pokedex4.self_link
+}
+
 resource "random_id" "db_name_suffix" {
   byte_length = 4
 }
 
 resource "google_sql_user" "users" {
   name     = "jishnn"
-  instance = google_sql_database_instance.pokedex3.self_link
+  instance = google_sql_database_instance.pokedex4.self_link
   host     = "wordpress.com"
   password = "324b21"
 }
-*/
-
