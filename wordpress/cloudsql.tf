cf99f18f75591068ff4b90bed15eacccd92ef346
jishnn tp <jishnn.tp@gmail.com>
2021-02-18 14:35:38 +0530

    sql conf

--- wordpress/cloudsql.tf
+++ wordpress/cloudsql.tf
@@ -32,34 +32,3 @@ resource "google_sql_user" "users" {
   host     = "wordpress.com"
   password = "324b21"
 }
-
-/*
-
-resource "google_sql_database" "main" {
-  name     = "wordpressdb"
-  instance = google_sql_database_instance.Apokedex.name
-}
-
-resource "google_sql_database_instance" "Apokedex" {
-  name             = "wordpress"
-  database_version = "MYSQL"
-  depends_on       = [var.db_depends_on]
-
-  settings {
-    tier              = "db-f1-micro"
-    availability_type = "REGIONAL"
-    disk_size         = "10"
-
-    ip_configuration {
-      ipv4_enabled    = false        # don't give the db a public IPv4
-      private_network = google_compute_network.Apokenav.self_link # the VPC where the db will be assigned a private IP
-    }
-  }
-}
-
-resource "google_sql_user" "db_user" {
-  name     = "jishnn"
-  instance = google_sql_database_instance.Apokedex.name
-  password = var.password
-}
-*/
